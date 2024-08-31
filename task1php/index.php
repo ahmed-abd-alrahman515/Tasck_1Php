8d28ac4d3440a85da1132e1296d71b0be6a07fcf
ahmed-alayde <146164820+ahmed-abd-alrahman515@users.noreply.github.com>
2024-08-31 16:11:23 +0400

    Add files via upload

---
+++ task1php/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$day = "Wednesday";
+
+
+if($day == "sunday"){
+    echo "الاحد";
+}else if ($day == "monday"){
+  echo "الاثنين";
+}else if($day == "tuesday"){
+echo "الثلاثاء";
+}else if($day == "Wednesday"){
+    echo "الاربعاء";
+    }else if($day == "thusday"){
+        echo "الخميس";
+        }else if($day == "friday"){
+            echo "الجمعه";
+            }
+            
+        
+    
+
+
+?>
